cf8b634008f79ab5cdb6a3726a316fe4df170c96
Administrator <Administrator@DESKTOP-MRVQ7GL>
2020-12-21 16:53:47 +0530

    Added Search Bar

--- src/app/country/country.component.ts
+++ src/app/country/country.component.ts
@@ -2,6 +2,25 @@ import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
 
+export class Countrys {
+  title = 'app-country';
+  searchText;
+  heroes = [
+    { id: 11, name: 'Mr. Nice', country: 'India' },
+    { id: 12, name: 'Narco' , country: 'USA'},
+    { id: 13, name: 'Bombasto' , country: 'UK'},
+    { id: 14, name: 'Celeritas' , country: 'Canada' },
+    { id: 15, name: 'Magneta' , country: 'Russia'},
+    { id: 16, name: 'RubberMan' , country: 'China'},
+    { id: 17, name: 'Dynama' , country: 'Germany'},
+    { id: 18, name: 'Dr IQ' , country: 'Hong Kong'},
+    { id: 19, name: 'Magma' , country: 'South Africa'},
+    { id: 20, name: 'Tornado' , country: 'Sri Lanka'}
+  ];
+}
+
+
+
 export class Country{
 
   constructor(
@@ -45,9 +64,25 @@ export class CountryComponent implements OnInit {
         }
       );
       }
-      
+      search(){
+        if(this.Country == ""){
+          this.ngOnInit();
+        }else{
+          this.country =this.country.filter(res =>{
+            return res.Country.toLocaleLowerCase().match(this.Country.toLocaleLowerCase());
+          });
+        }
+      }
 }
 
+  //   this.http.get('https://restcountries.eu/rest/v2/all')
+  //   .subscribe (Response => { 
+  
+  //     // If response comes hideloader() function is called 
+  //     // to hide that loader  
+  //     if(Response){   
+  //       hideloader(); 
+  //     } 
  
   
 
